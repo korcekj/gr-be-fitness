55f07077606ef949ef28ecac12dc8ebddb060290
Ján Korček <korcekdevelopment@gmail.com>
2025-01-20 13:28:00 +0100

    feat: add sign up schema

---
+++ src/utils/schemas.ts
@@ -0,0 +1,16 @@
+import { z } from 'zod';
+import { hashPassword } from './';
+import { USER_ROLE } from './enums';
+
+export const signUpSchema = z
+  .object({
+    email: z.string().email(),
+    password: z.string().min(6),
+    role: z.nativeEnum(USER_ROLE).optional(),
+  })
+  .transform((data) => {
+    return {
+      ...data,
+      password: hashPassword(data.password),
+    };
+  });
