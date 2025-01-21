2bfb6a47edc9fcb77b612058e24a7398198b773f
Ján Korček <korcekdevelopment@gmail.com>
2025-01-21 12:34:31 +0100

    feat: add custom error

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -1,11 +1,17 @@
 import { NextFunction, Request, Response } from 'express';
 
+import { HTTPError } from '../utils/errors';
+
 export const errorHandler = (
   err: Error,
   _req: Request,
   res: Response,
   _next: NextFunction
 ) => {
+  if (err instanceof HTTPError) {
+    return res.status(err.status).send({ data: {}, message: err.message });
+  }
+
   console.error(err);
   res.status(500).send({ data: {}, message: 'Something went wrong' });
 };
