e511174607c22bc0a9783604491075e6a5405183
Ján Korček <korcekdevelopment@gmail.com>
2025-01-22 21:28:31 +0100

    refactor: change method of adding exercise to program

--- src/routes/programs.ts
+++ src/routes/programs.ts
@@ -55,7 +55,7 @@ export default () => {
     }
   );
 
-  router.patch(
+  router.post(
     '/:programID/exercises/:exerciseID',
     verifyAuth(USER_ROLE.ADMIN),
     processRequestParams(updateProgramExerciseSchema),
