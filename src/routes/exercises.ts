082bb93cd0d2b69b88dc437fdcefcad2d7d698cc
Ján Korček <korcekdevelopment@gmail.com>
2025-01-22 19:43:45 +0100

    feat: add filtering and pagination to exercises

--- src/routes/exercises.ts
+++ src/routes/exercises.ts
@@ -1,27 +1,48 @@
+import { Op } from 'sequelize';
 import { Router, Request, Response, NextFunction } from 'express';
+import { processRequestQuery, TypedRequestQuery } from 'zod-express-middleware';
 
 import { models } from '../db';
+import { getExercisesSchema } from '../utils/schemas';
 
 const router: Router = Router();
 
 const { Exercise, Program } = models;
 
 export default () => {
-  router.get('/', async (_req: Request, res: Response, _next: NextFunction) => {
-    const exercises = await Exercise.findAll({
-      include: [
-        {
-          model: Program,
-          as: 'program',
-        },
-      ],
-    });
+  router.get(
+    '/',
+    processRequestQuery(getExercisesSchema),
+    async (
+      req: TypedRequestQuery<typeof getExercisesSchema>,
+      res: Response,
+      _next: NextFunction
+    ) => {
+      const { page, limit, programID, search } = req.query;
 
-    return res.json({
-      data: exercises,
-      message: res.__('messages.exercise.list'),
-    });
-  });
+      const where = {
+        ...(programID && { programID }),
+        ...(search && { name: { [Op.iLike]: `%${search}%` } }),
+      };
+
+      const exercises = await Exercise.findAll({
+        where,
+        limit,
+        offset: page ? (page - 1) * limit! : undefined,
+        include: [
+          {
+            model: Program,
+            as: 'program',
+          },
+        ],
+      });
+
+      return res.json({
+        data: exercises,
+        message: res.__('messages.exercise.list'),
+      });
+    }
+  );
 
   return router;
 };
